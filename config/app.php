ffc831e7aa24156ee46f44a8f5747bb36d619417
Daniel <da130502@gmail.com>
2024-10-08 20:58:27 -0600

    Arquitectura

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+
+	const APP_URL="http://localhost/CRUD/";
+	const APP_NAME="GGP";
+	const APP_SESSION_NAME="GGP";
+
+
+	/*----------  Zona horaria  ----------*/
+	date_default_timezone_set("America/Mexico_City");
+
+	/*
+		Configuración de zona horaria de tu país, para más información visita
+		http://php.net/manual/es/function.date-default-timezone-set.php
+		http://php.net/manual/es/timezones.php
+	*/
