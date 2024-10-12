41fe9b3dbbda4df04d4a56aa0f1a81577de983cf
Daniel <da130502@gmail.com>
2024-10-11 19:56:36 -0600

    Esqueleto de los demas CRUD

---
+++ src/controllers/movimientos/controller-movimientos.js
@@ -0,0 +1,36 @@
+const db = require('../../DB/db2');
+
+const TABLA = 'movimientos';
+
+
+module.exports = function (dbInyectada){
+    let db = dbInyectada;
+
+    if(!db){
+        db = require('../../DB/db2');
+    }
+
+    function todos (){
+        return db.todos(TABLA);
+    }
+    
+    function uno (id){
+        return db.uno(TABLA,id);
+    }
+    
+    function agregar (body){
+        return db.agregar(TABLA, body);
+    }
+    
+    function eliminar (body){
+        return db.eliminar(TABLA, body);
+    }
+
+    return {
+        
+        todos,
+        uno,
+        agregar,
+        eliminar,
+    }
+}
---
+++ src/controllers/movimientos/index.js
@@ -0,0 +1,4 @@
+const db = require('../../DB/db');
+const ctrl = require('./controller-tarjetas');
+
+module.exports = ctrl (db);
